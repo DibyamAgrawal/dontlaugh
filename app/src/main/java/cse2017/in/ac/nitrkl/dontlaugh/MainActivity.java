96a7f08f5d34ed1c74f37badf8b84ca0dea83df2
Dibyam <dibyam.agrawal@gmail.com>
2017-10-22 07:10:02 +0530

    Initial commit

---
+++ app/src/main/java/cse2017/in/ac/nitrkl/dontlaugh/MainActivity.java
@@ -0,0 +1,13 @@
+package cse2017.in.ac.nitrkl.dontlaugh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
